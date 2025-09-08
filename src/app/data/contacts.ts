e2140be2b8162e9c3a8422a2f84277308d41fe17
Jakub Mroz <kuba.mroz01@gmail.com>
2025-09-08 10:53:15 +0200

    feat: init angular setup

---
+++ src/app/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from '../models/contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstName: 'Florian',
+    lastName: 'Berg',
+    street: 'Pavington 5',
+    city: 'Chelsea',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Mayer',
+    street: 'Langgata 12',
+    city: 'Sandnes',
+  },
+];
